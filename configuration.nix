cf8784f35c1d1f48f3b395639c9604e612f7ff77
stefiii <stefanundervover20@gmail.com>
2024-04-15 16:57:59 +0200

    aha

---
+++ configuration.nix
@@ -0,0 +1,167 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    /etc/nixos/hardware-configuration.nix
+  ];
+
+  # paste your boot config here...
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking = {
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [];
+      allowedUDPPorts = [];
+    };
+    hostName = "Stefiii";
+    networkmanager.enable = true;
+  };
+
+  # edit as per your location and timezone
+  time.timeZone = "Europe/Vienna";
+  i18n = {
+    defaultLocale = "de_AT.utf8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "de_AT.utf8";
+      LC_IDENTIFICATION = "de_AT.utf8";
+      LC_MEASUREMENT = "de_AT.utf8";
+      LC_MONETARY = "de_AT.utf8";
+      LC_NAME = "de_AT.utf8";
+      LC_NUMERIC = "de_AT.utf8";
+      LC_PAPER = "de_AT.utf8";
+      LC_TELEPHONE = "de_AT.utf8";
+      LC_TIME = "de_AT.utf8";
+      LC_CTYPE="en_US.utf8"; # required by dmenu don't change this
+    };
+  };
+
+  sound.enable = true;
+
+  services = {
+    xserver = {
+      layout = "de";
+      xkbVariant = "";
+      enable = true;
+      windowManager.i3 = {
+        enable = true;
+        extraPackages = with pkgs; [
+          i3status
+        ];
+      };
+      desktopManager = {
+        xterm.enable = false;
+        xfce = {
+          enable = true;
+          noDesktop = true;
+          enableXfwm = false;
+        };
+      };
+      displayManager = {
+        lightdm.enable = true;
+        defaultSession = "xfce+i3";
+      };
+    };
+    gvfs.enable = true;
+    gnome.gnome-keyring.enable = true;
+    blueman.enable = true;
+    pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse.enable = true;
+    };
+  };
+
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      pulseaudio = true;
+    };
+  };
+
+  # Edit the username below (replace 'neeraj')
+  users.users.stefiii = {
+    isNormalUser = true;
+    description = "stefiii";
+    extraGroups = [ "networkmanager" "wheel"];
+    packages = with pkgs; [
+      xarchiver
+    ];
+  };
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  environment.systemPackages = with pkgs; [
+    alacritty
+    dmenu
+    git
+    nerdfonts
+    networkmanagerapplet
+    nitrogen
+    pasystray
+    picom
+    polkit_gnome
+    pulseaudioFull
+    rofi
+    vim
+    zip
+    unrar
+    unzip
+    neovim
+    signal-desktop
+    zsh
+    fzf
+    steam
+    pkgs.libreoffice
+    pkgs.texliveMedium
+    pkgs.zathura
+    pkgs.tree-sitter-grammars.tree-sitter-latex
+    (python3.withPackages (ps: with ps; [ pandas ]))
+    pkgs.nodejs_21
+  ];
+  programs.neovim = {
+    enable = true;
+    withPython3 = true;
+    defaultEditor = true;
+  };
+
+  programs.steam.enable = true;
+  nix.optimise.automatic = true;
+  nix.settings.auto-optimise-store = true;
+  services.flatpak.enable = true;
+  xdg.portal.enable = true;
+  programs = {
+    thunar.enable = true;
+    dconf.enable = true;
+  };
+
+  security = {
+    polkit.enable = true;
+    rtkit.enable = true;
+  };
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart =
+          "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+  hardware = {
+    bluetooth.enable = true;
+  };
+
+  # Don't touch this
+  system.stateVersion = "23.05";
+}
