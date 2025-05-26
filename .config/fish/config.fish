3df9d825d91c592229a2fd39c266f22d9a99d2a5
Stefanistkuhl <ghgstefan@gmail.com>
2025-05-26 09:35:01 +0200

    added fish plugins and changed clipboard history

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,19 +1,21 @@
 set fish_greeting ""
 # figlet -n "oida" | cowsay -n
 set -x EDITOR nvim
+set -x GOPATH ~/.go
 
 if status is-interactive
+
+    zoxide init --cmd cd fish | source
+    starship init fish | source
+
     alias clock='tty-clock -sc'
     alias neovim='nvim'
     alias v='nvim'
     alias vim='nvim'
     alias vd='cd /home/stefiii/dotfiles && nvim .'
-    alias history='history 1'
     alias ff='hyfetch -b fastfetch'
     alias :3='hyfetch -b fastfetch'
     alias yay='paru'
-    zoxide init --cmd cd fish | source
-    starship init fish | source
     alias putty='sudo cu -l /dev/ttyUSB0 -s 9600'
     alias ls='eza'
     alias t='tmux attach -t blank || tmux new-session -s blank'
@@ -26,5 +28,7 @@ if status is-interactive
     alias gr='git reset --hard HEAD'
     alias cummies='git push'
     alias booba='git pull'
+    alias dcps="nu -c 'docker compose ps | detect columns'"
+    alias dps="nu -c 'docker ps | detect columns'"
     # skim_key_bindings
 end
