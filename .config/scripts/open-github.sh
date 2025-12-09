662cdca740bb3f53498db8dab39ae969e3e19868
Stefanistkuhl <ghgstefan@gmail.com>
2025-12-09 11:28:28 +0100

    added kb layout

--- .config/scripts/open-github.sh
+++ .config/scripts/open-github.sh
@@ -3,7 +3,7 @@
 cd $(tmux run "echo #{pane_start_path}")
 url=$(git remote get-url origin) 
 
-if [[ $url == *github.com* ]]; then
+if [[ $url == *github.com* ]] || [[ $url == *codeberg.org* ]]; then
     if [[ $url == git@* ]]; then
         url="${url#git@}"
         url="${url/:/\/}" 
@@ -11,5 +11,5 @@ if [[ $url == *github.com* ]]; then
     fi
     xdg-open "$url"
 else
-    echo "This repository is not hosted on GitHub"
+    echo "This repository is not hosted on GitHub or Codeberg."
 fi
